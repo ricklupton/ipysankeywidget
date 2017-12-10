d118e1bcb86a7d9962b6151ee0d52ece316954f2
Rick Lupton <mail@ricklupton.name>
2017-12-10 00:00:12 +0000

    Update versions and example notebooks

--- ipysankeywidget/sankey_widget.py
+++ ipysankeywidget/sankey_widget.py
@@ -19,8 +19,8 @@ class SankeyWidget(widgets.DOMWidget):
     _model_name = Unicode('SankeyModel').tag(sync=True)
     _view_module = Unicode('jupyter-sankey-widget').tag(sync=True)
     _model_module = Unicode('jupyter-sankey-widget').tag(sync=True)
-    _view_module_version = Unicode('^0.2.2').tag(sync=True)
-    _model_module_version = Unicode('^0.2.2').tag(sync=True)
+    _view_module_version = Unicode('^0.2.3').tag(sync=True)
+    _model_module_version = Unicode('^0.2.3').tag(sync=True)
 
     links = List([]).tag(sync=True)
     nodes = List([]).tag(sync=True)
